3ab2405a476bf3885a3fb24eead3645a80eba082
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2025-08-07 14:54:06 +0200

    Initial commit of base extension

---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Gedankenfolger Viewhelper',
+    'description' => 'A collection of viewhelpers to make the work a little bit easier',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Gedankenfolger\\GedankenfolgerViewhelper\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Niels Tiedt',
+    'author_email' => 'niels.tiedt@gedankenfolger.de',
+    'author_company' => 'Gedankenfolger',
+    'version' => '1.0.0',
+];
