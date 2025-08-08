24082ed78f5249ba0a13073c0508503ba6e469c5
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2025-08-08 09:50:05 +0200

    Add global namespace gfv

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,6 @@
 <?php
 
 defined('TYPO3') or die('Access denied.');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['gfv'][] =
+    'Gedankenfolger\\GedankenfolgerViewhelper\\ViewHelpers';
