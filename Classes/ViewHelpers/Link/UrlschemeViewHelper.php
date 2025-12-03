2f5108f73cd89993fa5f4732461921a79d021b1f
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2025-12-03 14:37:55 +0100

    Update phone number validation regex and removal of special characters.

--- Classes/ViewHelpers/Link/UrlschemeViewHelper.php
+++ Classes/ViewHelpers/Link/UrlschemeViewHelper.php
@@ -18,8 +18,11 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
  * Example usage:
  * <gfv:link.urlscheme number="+49 (0) 7777 77 77 77" />
  *
+ * Exmple numbers:
+ * +49 2817 333-278, +49 2817 333-459, +49 2817 333-661, +49 2817 333-297, +49 2817 333-8090, +49 2817 333-250, +49 2817 333-233, +49 2817 333-210, +49 2817 333-262, +49 2817 333-610, +49 (0) 7777 77 77 77, +1 973 914 1306, +1 862 200 8073, +1 862 345 0739, +86 158 2141 5697, +33 6 32 85 82 98, +91 9146003489, +65 9746 0800
+ *
  * @package Gedankenfolger\GedankenfolgerViewhelper\ViewHelpers\Link
- * @version 13.0.4
+ * @version 13.0.5
  * @since 12.2.0
  * @author    Niels Tiedt <niels.tiedt@gedankenfolger.de>
  * @company   Gedankenfolger GmbH
@@ -62,12 +65,12 @@ final class UrlschemeViewHelper extends AbstractTagBasedViewHelper
         $number = $this->arguments['number'];
 
         // Define a regex pattern to validate international phone number formats
-        $pattern = '/^\+(\d{1,3})\s?(\(0\))?\s?\d{1,4}[\s.-]?\d{1,4}[\s.-]?\d{1,4}[\s.-]?\d{1,4}$/';
+        $pattern = '/^\+\d{1,3}(?:\s?\(0\))?(?:[\s-]?\d+)+$/';
 
         // Check if the phone number matches the expected format
         if (preg_match($pattern, $number)) {
             // Format the phone number by removing spaces, parentheses, and dashes
-            $formattedNumber = preg_replace('/\s+|\(0\)|[\s.-]/', '', $number);
+            $formattedNumber = preg_replace('/\s+|\(0\)|[.\-]/', '', $number);
         } else {
             // Throw an exception if the phone number is in an invalid format
             throw new Exception('ERROR: Invalid format. Required format e.g. +49 (0) 7777 77 77 77', 1700485661);
