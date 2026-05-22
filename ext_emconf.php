9770458778fa58752fd96e295c3e284950afbb68
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-05-22 19:42:27 +0200

    feat(ter): add ext_emconf.php for TER publishing

---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$_EXTKEY = 'gedankenfolger_viewhelper';
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Gedankenfolger Viewhelper',
+    'description' => 'A collection of viewhelpers to make the work a little bit easier',
+    'category' => 'misc',
+    'author' => 'Niels Tiedt, Gedankenfolger GmbH',
+    'author_email' => 'niels.tiedt@gedankenfolger.de',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'version' => '14.0.5',
+    'autoload' => [
+        'psr-4' => [
+            'Gedankenfolger\\GedankenfolgerViewhelper\\' => 'Classes',
+        ],
+    ],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '14.0.0-14.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
