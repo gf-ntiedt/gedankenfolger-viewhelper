50b0c96086646b263147234785840aff0c85ef25
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-05-26 14:58:58 +0200

    chore(release): bump version to 13.2.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,5 +25,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Niels Tiedt',
     'author_email' => 'niels.tiedt@gedankenfolger.de',
     'author_company' => 'Gedankenfolger GmbH',
-    'version' => '13.2.5',
+    'version' => '13.2.6',
 ];
